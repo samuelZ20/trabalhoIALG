e00d8c203ff20e925303c60b4771a1d764917294
samuelZ20 <frizzonecardoso@gmail.com>
2025-06-05 15:43:27 -0300

    Update FILMES.cpp

--- FILMES.cpp
+++ FILMES.cpp
@@ -12,6 +12,14 @@ struct Filmes{
     string sinopse;
 };
 
+void moldura(filmes f){
+ cout << "★══════════════════════════════════════════════════════════════════════★"<< endl;
+    cout << "|Filme: " << f.nome << endl ;
+    cout << "|Diretor: " << f.diretor << endl;
+    cout << "|Ano do Oscar: " << f.ano << endl;
+    cout << "|Sinopse: " << f.sinopse << endl; 
+    cout << "★══════════════════════════════════════════════════════════════════════★" << endl;
+}
 
 int contarlinhas(string linha){
     int cont = 0;
